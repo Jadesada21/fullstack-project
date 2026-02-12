22c8ee0a23ec6d0ee896bfda228a79faf1a81e5c
jadesada <jadesada.jung@gmail.com>
2026-02-12 15:49:45 +0700

    Cancel use MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv'
-import connectDb from './mongodb/connect'
 import app from './app'
 import { pool } from './db/connectPostgre'
 
@@ -9,13 +8,6 @@ dotenv.config()
 const startServer = async () => {
     try {
         const PORT = Number(process.env.PORT) || 3000
-
-        const MONGO_URL = process.env.MONGODB_URL;
-        if (!MONGO_URL) {
-            throw new Error("MONGODB_URL is not defined");
-        }
-
-        await connectDb(MONGO_URL)
 
         await pool.query("SELECT 1")
         console.log("PostgreSQL ready")
